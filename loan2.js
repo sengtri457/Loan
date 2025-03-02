4696e9bf5b6627de2bd406bfeb0ed26a69b8d89a
Bun Sengtri <157777973+sengtri457@users.noreply.github.com>
2025-03-02 20:37:07 +0700

    Add files via upload

---
+++ loan2.js
@@ -0,0 +1,10 @@
+const price = 2000;
+const duration = 10;
+const interestRate = 2 / 100;
+
+let pricePaid = 0;
+let totalAmountPaid = 0;
+let TotalInteerst = 0;
+let day = "25",
+  month = "10",
+  year = "2023";
